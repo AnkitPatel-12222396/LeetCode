e8a429a3f4ad231d63dfa38fa234ee6ec9ce7d3e
Ankit Patel <work.ankitpatel1@gmail.com>
2025-02-02 23:28:20 +0530

    Time: 0 ms (100%), Space: 43.2 MB (84.29%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        // if(nums.length==0 || nums.length==1) return true;
-        if(nums.length==2){
-            return (nums[0]+nums[1])%2!=0;
-        }
-        for(int i=1;i<nums.length-1;i++){
-            if((nums[i]+nums[i-1])%2!=0 && (nums[i]+nums[i+1])%2!=0) continue;
-            else return false;
+        if (nums.length <= 1) return true;
 
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] % 2 == nums[i - 1] % 2) {
+                return false;
+            }
         }
         return true;
-
     }
 }
